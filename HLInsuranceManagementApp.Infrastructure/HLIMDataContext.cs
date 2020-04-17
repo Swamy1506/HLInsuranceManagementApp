1e4fbace91136555d4a5ebcc8183da88fa72ecec
Apkolla.Swamy <Apkolla.Swamy@ASWAMY03>
2020-04-18 00:13:29 +0530

    Initial setup for project

---
+++ HLInsuranceManagementApp.Infrastructure/HLIMDataContext.cs
@@ -0,0 +1,27 @@
+﻿using HLInsuranceManagementApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HLInsuranceManagementApp.Infrastructure
+{
+    public class HLIMDataContext : DbContext
+    {
+        public HLIMDataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Borrower> Borrowers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Borrower>(x =>
+            {
+                x.ToTable("Borrower").HasKey(k => k.Id);
+                x.Property(p => p.Id).HasColumnName("BorrowerId");
+            });
+        }
+    }
+}
---
+++ HLInsuranceManagementApp.Infrastructure/Migrations/20200417180255_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HLInsuranceManagementApp.Infrastructure.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Borrower",
+                columns: table => new
+                {
+                    BorrowerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Gender = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    StreetAddress = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Borrower", x => x.BorrowerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Borrower");
+        }
+    }
+}
