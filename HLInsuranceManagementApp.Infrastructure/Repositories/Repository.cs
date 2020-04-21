2aa10a789f466b4706880b26760d5ef6ef9b477d
Apkolla.Swamy <Apkolla.Swamy@ASWAMY03>
2020-04-21 10:31:48 +0530

    returned newly created Id

--- HLInsuranceManagementApp.Infrastructure/Repositories/Repository.cs
+++ HLInsuranceManagementApp.Infrastructure/Repositories/Repository.cs
@@ -17,38 +17,63 @@ namespace HLInsuranceManagementApp.Infrastructure.Repositories
             Context = context;
         }
 
+        /// <summary>
+        /// Add dynamic entity to database
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task<int> Add(TEntity entity)
         {
-            var newId = 0;
-            Context.Set<TEntity>().Add(entity);
+            await Context.Set<TEntity>().AddAsync(entity);
+
             await Context.SaveChangesAsync();
 
-             //entity.GetType().GetProperty("Id").SetValue(entity, newId);
-
-            return newId;
+            return Convert.ToInt32(entity.GetType().GetProperty("Id").GetValue(entity)); ;
         }
 
+        /// <summary>
+        /// Add collection of entities to database
+        /// </summary>
+        /// <param name="entities"></param>
         public void AddRange(IEnumerable<TEntity> entities)
         {
             Context.Set<TEntity>().AddRange(entities);
-
+            Context.SaveChanges();
         }
 
+        /// <summary>
+        /// Get entity from db using id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public TEntity Get(int id)
         {
             return Context.Set<TEntity>().Find(id);
         }
 
+        /// <summary>
+        /// Get all entity values from db
+        /// </summary>
+        /// <returns></returns>
         public List<TEntity> GetAll()
         {
             return Context.Set<TEntity>().ToList();
         }
 
+        /// <summary>
+        /// Delete entity from db
+        /// </summary>
+        /// <param name="entity"></param>
         public void Remove(TEntity entity)
         {
             Context.Set<TEntity>().Remove(entity);
+            Context.SaveChanges();
         }
 
+        /// <summary>
+        /// Remove collection of entities from db
+        /// </summary>
+        /// <param name="entities"></param>
         public void RemoveRange(IEnumerable<TEntity> entities)
         {
             Context.Set<TEntity>().RemoveRange(entities);
