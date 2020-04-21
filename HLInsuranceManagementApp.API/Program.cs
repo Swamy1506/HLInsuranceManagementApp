2794da99d2c488176fa2e2d61ad8100613ebe688
Apkolla.Swamy <Apkolla.Swamy@ASWAMY03>
2020-04-21 19:54:31 +0530

    Implemented seed data for bank,companies tables

--- HLInsuranceManagementApp.API/Program.cs
+++ HLInsuranceManagementApp.API/Program.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HLInsuranceManagementApp.Infrastructure;
+using HLInsuranceManagementApp.Infrastructure.SeedData;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,19 +18,30 @@ namespace HLInsuranceManagementApp.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<HLIMDataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedData(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                    .ConfigureLogging(logging =>
-                    {
-                        logging.ClearProviders();
-                        logging.AddConsole();
-                    })
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.UseStartup<Startup>();
-                    });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+         WebHost.CreateDefaultBuilder(args)
+             .UseStartup<Startup>();
+
     }
 }
---
+++ HLInsuranceManagementApp.Infrastructure/SeedData/Seed.cs
@@ -0,0 +1,108 @@
+﻿using HLInsuranceManagementApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HLInsuranceManagementApp.Infrastructure.SeedData
+{
+    public class Seed
+    {
+
+
+
+        public static void SeedData(HLIMDataContext context)
+        {
+
+            if (!context.Banks.Any())
+            {
+                var banks = new List<Bank>()
+                {
+                    new Bank()
+                    {
+                        Name = "SBI"
+                    },
+                    new Bank()
+                    {
+                        Name = "HDFC"
+                    },
+                    new Bank()
+                    {
+                        Name = "ICICI"
+                    }
+                };
+                context.Banks.AddRange(banks);
+            }
+
+            if (!context.InsuranceCompanies.Any())
+            {
+                var banks = new List<InsuranceCompany>()
+                {
+                    new InsuranceCompany()
+                    {
+                        CompanyName = "SBILife"
+                    },
+                    new InsuranceCompany()
+                    {
+                        CompanyName = "HDFCLife"
+                    },
+                    new InsuranceCompany()
+                    {
+                        CompanyName = "ICICILife"
+                    },
+                };
+                context.InsuranceCompanies.AddRange(banks);
+                context.SaveChanges();
+            }
+
+            if (!context.InsuranceCompanies.Any())
+            {
+                var companies = new List<InsuranceCompany>()
+                {
+                    new InsuranceCompany()
+                    {
+                        CompanyName = "SBILife"
+                    },
+                    new InsuranceCompany()
+                    {
+                        CompanyName = "HDFCLife"
+                    },
+                    new InsuranceCompany()
+                    {
+                        CompanyName = "ICICILife"
+                    },
+                };
+                context.InsuranceCompanies.AddRange(companies);
+                context.SaveChanges();
+            }
+
+            if (!context.InsurancePolicies.Any())
+            {
+                var policies = new List<InsurancePolicy>()
+                                    {
+                                        new InsurancePolicy()
+                                        {
+                                            PremiumAmount = 10000,
+                                            Tenure = 10,
+                                            CompanyId =1
+                                        },
+                                        new InsurancePolicy()
+                                        {
+                                            PremiumAmount = 12000,
+                                            Tenure = 8,
+                                            CompanyId =1
+                                        },
+                                        new InsurancePolicy()
+                                        {
+                                           PremiumAmount = 8000,
+                                           Tenure = 13,
+                                           CompanyId =1
+                                        }
+                                    };
+                context.InsurancePolicies.AddRange(policies);
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
